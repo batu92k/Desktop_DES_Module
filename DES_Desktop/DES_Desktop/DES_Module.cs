6e5b59f1e68e8a1cbd2d7247d6918926003efc7b
Batu <batuhan.kindan@gmail.com>
2017-12-12 13:52:29 +0300

    DES modül dosyası oluşturuldu
    DES sınıfı, constructor ve destructor fonksiyonları ile birlikte
    tanımlandı.

---
+++ DES_Desktop/DES_Desktop/DES_Module.cs
@@ -0,0 +1,54 @@
+﻿/**
+  ******************************************************************************
+  * @file    DES_Module.cs
+  * @author  Ali Batuhan KINDAN
+  * @version V0.1
+  * @date    12 Aralik 2017
+  * @brief   Data Encryption Standart (DES) sinifinin global gedigkenleri, constructor,
+  *          destructor ve kriptolama islem fonksiyonlari bu dosyada bulunur. 
+  ******************************************************************************
+  */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DES_Module
+{
+
+    public class DES
+    {
+
+
+        /**
+          * @brief  DES (Data Encryption Standart) sinif constructor fonksiyonu
+          * @param  none
+          * @retval none
+          */
+        public DES()
+        {
+
+        }
+
+
+        /**
+          * @brief  DES (Data Encryption Standart) sinif destructor fonksiyonu
+          * @param  none
+          * @retval none
+          */
+        ~DES()
+        {
+
+        }
+
+
+
+    }
+
+}
